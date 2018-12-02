731dfe08b915ab14b49b9085e5620cfab9eaceee
tiramisuyj <tiramisuyj@163.com>
2018-12-02 23:06:17 +0800

    初始化数据

---
+++ src/main/java/com/cnc/springbootstep/SpringbootstepApplication.java
@@ -0,0 +1,12 @@
+package com.cnc.springbootstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstepApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cnc/springbootstep/SpringbootstepApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cnc.springbootstep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootstepApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
